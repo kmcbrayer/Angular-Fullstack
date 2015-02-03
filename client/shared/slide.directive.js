53ed0cf20dca5c105914ef6f62008ccb9d5d938b
Kevin McBrayer <kmcbrayer77@gmail.com>
2015-02-03 09:11:55 -0500

    locked viewport for sliding on mobile devices

--- client/shared/slide.directive.js
+++ client/shared/slide.directive.js
@@ -7,34 +7,32 @@ angular.module('angularFullstackApp')
       link: function(scope,ele,attrs,ctrl) {
         function slideBack() {
           ele.animate({left: 0},400,'swing');
-          time = 0;
         }
         var width = Math.max( $(window).width(), window.innerWidth);
-        if (width <= 780 ) {
+        if (width <= 762 ) {
           var startx, delta;
           var enabled = true;
-          var time = 0;
+          //lock the viewport for sliding on mobile devices
+          $('.container').css('max-width', width);
+          $('.container').css('width', width);
+          $('.container').css('overflow', 'hidden');
           $swipe.bind(ele, {
             'start': function(coords) {
               //reset for next swipe
               enabled = true;
               startx = coords.x;
-              window.setInterval(function() {
-                time+=100;
-              }, 100);
             },
             'move': function(coords) {
               delta = coords.x - startx;
               if (enabled) {
                 ele.css('left', delta);
-                if (coords.x < 50 ) {
+                if (coords.x < 50 || delta <= -150 ) {
                   enabled = false;
                   //animate left
                   pageSetService.pageTurnRight();
-                  //$('.first').animate({})
                   $rootScope.$broadcast('pageTurn');
                 }
-                if (coords.x > width-50) {
+                if (coords.x > width-50 || delta >= 150) {
                   enabled = false;
                   //animate right
                   pageSetService.pageTurnLeft();
