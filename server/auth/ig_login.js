65530e9d0b692a899fa050bbd1661b802ccf7bea
kmcbrayer <kmcbrayer77@gmail.com>
2015-02-16 22:16:36 -0500

    instagram login issues resolved

--- server/auth/ig_login.js
+++ server/auth/ig_login.js
@@ -2,6 +2,7 @@
 
 var InstagramStrategy = require('passport-instagram').Strategy;
 var secrets = require('../config/secrets');
+var config = require('../config/environment');
 var log = require('../config/winston');
 var passport = require('passport')
 
