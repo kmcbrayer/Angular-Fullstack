612d75508ff137bd8607364abcd3a116a841a930
Kevin McBrayer <kmcbrayer77@gmail.com>
2016-05-11 23:18:20 -0400

    delete keys

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,18 +1,1 @@
-'use strict';
 
-exports.twitter = {
-  consumerKey : 'nBDpKjVXPtYTrOUDebWB6TZ8Y',
-  consumerSecret : 'dgMop9KO1iUQvKzhnNGySMcnmC2HdFGKrktoN70oWzvZELU6I1',
-  accessToken : '124310203-Fn9Ta0f8WJsyHgexDegXZYesH4cTKYNtjZ2m4oiB',
-  accessSecret : 'BNfm0kRw1MiU3wtqE5uNlMXRT6LX82b3U4U2gytrBjLOp'
-}
-
-exports.youtube = {
-  app_id : '710646847960-d59j2e3ukdpnj6n506uvblljtqvqhpdg.apps.googleusercontent.com',
-  app_secret : 'bkytyg-JGudvW55DNjU4XYiT'
-}
-
-exports.instagram = {
-  app_id: 'f077ada3299249d996fd17b092dec11b',
-  app_secret: '855c64806d1f4e2ca7b9daf1edcfc814'
-}
